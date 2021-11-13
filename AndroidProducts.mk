df2c5470f1726a5d5a3b30e2f0537c7d5aa1cd41
Nazim N <79507925+Nazimn08@users.noreply.github.com>
2021-11-13 10:47:22 +0530

    AndroidProducts.mk updated

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,6 +14,6 @@
 # limitations under the License.
 #
 
-PRODUCT_MAKEFILES := $(LOCAL_DIR)/twrp_m21.mk
+PRODUCT_MAKEFILES := $(LOCAL_DIR)/omni_m21.mk
 
-COMMON_LUNCH_CHOICES := twrp_m21-eng
+COMMON_LUNCH_CHOICES := omni_m21-eng
