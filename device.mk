45a83c21c50f2cbbbb0d802f47b43f80008d75be
Nazim N <79507925+Nazimn08@users.noreply.github.com>
2021-11-13 11:21:26 +0530

    device.mk updated

--- device.mk
+++ device.mk
@@ -3,5 +3,13 @@ PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # fastbootd
 PRODUCT_PACKAGES += \
-    fastbootd \
-    android.hardware.fastboot@1.0-impl-mock
+    fastbootd
+
+# PRODUCT_PACKAGES += \
+#    android.hardware.fastboot@1.0-impl-mock \
+#    android.hardware.fastboot@1.0-impl-mock.recovery
+
+# Apex Libraries
+PRODUCT_HOST_PACKAGES += \
+    libandroidicu
+
