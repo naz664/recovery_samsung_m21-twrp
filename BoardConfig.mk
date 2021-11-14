d65f123f6240f6a1631a04f49ece94cdf7d1e2a6
Nazim N <79507925+nazim008@users.noreply.github.com>
2021-11-14 13:06:55 +0530

    Updated

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -27,6 +27,9 @@ TARGET_2ND_CPU_ABI := armeabi-v7a
 TARGET_2ND_CPU_ABI2 := armeabi
 TARGET_2ND_CPU_VARIANT := cortex-a53
 
+ENABLE_CPUSETS := true
+ENABLE_SCHEDBOOST := true
+
 # Bootloader
 BOARD_VENDOR := samsung
 TARGET_SOC := exynos9611
