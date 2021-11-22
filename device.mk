dcd1f47e8a358fe5c19694855b221ba2128d8a01
LMAO-armv8 <82850873+LMAO-armv8@users.noreply.github.com>
2021-11-22 13:42:14 +0530

    Fix implementation of pbrp

--- device.mk
+++ device.mk
@@ -2,8 +2,8 @@
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
 # fastbootd
-PRODUCT_PACKAGES += \
-    fastbootd
+# PRODUCT_PACKAGES += \
+#     fastbootd
 
 # PRODUCT_PACKAGES += \
 #    android.hardware.fastboot@1.0-impl-mock \
