c073eb3dd1a678aec14fcb5c68911b72165979b0
Nazim N <79507925+naz664@users.noreply.github.com>
2021-12-12 08:45:43 +0530

    Twrp bringup

--- twrp_m21.mk
+++ twrp_m21.mk
@@ -1,5 +1,8 @@
 # Inherit from the common Open Source product configuration
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+$(call inherit-product-if-exists, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
 # Include any options that can't be included in BoardConfig.mk
 $(call inherit-product, device/samsung/m21/device.mk)
